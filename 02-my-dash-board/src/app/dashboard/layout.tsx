f7ef6ac4523af03c9e7e8904770b7a375bc45347
EA-Gadgeter <emiacevedoposos@gmail.com>
2023-12-18 21:27:20 -0600

    Terminado layout con items semidinamicos

--- 02-my-dash-board/src/app/dashboard/layout.tsx
+++ 02-my-dash-board/src/app/dashboard/layout.tsx
@@ -9,7 +9,6 @@ interface Props {
 export default function DashboardLayout({ children }: Props) {
   return (
     <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
-      
       <div className="flex">
         <Sidebar />
 
---
+++ 02-my-dash-board/src/app/dashboard/main/page.tsx
@@ -0,0 +1,5 @@
+export default function MainCounterPage() {
+  return (
+    <div>Main page</div>
+  );
+}
