8fd0696dc1fe72cf9f5af39a565c38ab3e41e7fd
EA-Gadgeter <emiacevedoposo@gmail.com>
2023-12-27 23:31:50 -0600

    Agregada relacion entre todos y usuario, actualizadoa seed para que agregue todo y usuario

--- 03-admin-todos/README.md
+++ 03-admin-todos/README.md
@@ -5,16 +5,20 @@ Pasos para levantar app en desarrollo
 ```shell
 docker compose up -d
 ```
-1. Crear .env a partir del example
-2. Remplazar las variables de entorno
-3. Ejecutar el comando ```npm install```
-4. Ejecutar comandos de prisma:
+2. Crear .env a partir del example
+3. Remplazar las variables de entorno
+4. Ejecutar el comando ```npm install```
+5. Ejecutar comandos de prisma:
 ```shell
 npx prisma migrate dev # Crear las tablas en la base de datos
 npx prisma generate # Genere el cliente de prisma
 ```
-5. Ejecutar el comando ```npm run dev```
-6. Hacer petición a al seed para [generar](http://localhost:3000/api/seed) información de prueba en la base de datos
+6. Ejecutar el comando ```npm run dev```
+7. Hacer petición a al seed para [generar](http://localhost:3000/api/seed) información de prueba en la base de datos
+
+### Nota: Usuario por defecto
+__usuario__: test1@google.com
+__password__: 123456
 
 # Prisma commands
 ```shell
--- 03-admin-todos/src/app/api/seed/route.ts
+++ 03-admin-todos/src/app/api/seed/route.ts
@@ -1,11 +1,33 @@
 import { NextResponse } from "next/server";
+
+import bcrypt from "bcryptjs";
+
 import prisma from "@/lib/prisma";
+
 
 export const GET = async () => {
 
   await prisma.todo.deleteMany();
+  await prisma.user.deleteMany();
 
-  await prisma.todo.createMany({
+  const user = await prisma.user.create({
+    data: {
+      email: "test1@google.com",
+      password: bcrypt.hashSync("123456"),
+      roles: ["admin", "client", "super-user"],
+      todos: {
+        create: [
+          { description: "Piedra del alma", complete: true },
+          { description: "Piedra del tiempo" },
+          { description: "Piedra del espacio" },
+          { description: "Piedra del poder" },
+          { description: "Piedra del realidad" }
+        ]
+      }
+    }
+  });
+
+  /*await prisma.todo.createMany({
     data: [
       { description: "Piedra del alma", complete: true },
       { description: "Piedra del tiempo" },
@@ -13,7 +35,7 @@ export const GET = async () => {
       { description: "Piedra del poder" },
       { description: "Piedra del realidad" },
     ]
-  });
+  });*/
 
   return NextResponse.json({
     message: "Semilla ejecutada con éxito"
